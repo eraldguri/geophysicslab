efcbd94ebc2e1dd80117ca5d1b05bc530b82cdae
eraldguri <eraldguri@live.com>
2021-02-28 20:56:21 +0100

    fixing some issues with CSV

--- app/src/main/java/com/eraldguri/geophysicslab/fragments/tabs/EarthquakeListFragment.java
+++ app/src/main/java/com/eraldguri/geophysicslab/fragments/tabs/EarthquakeListFragment.java
@@ -1,18 +1,14 @@
 package com.eraldguri.geophysicslab.fragments.tabs;
 
-import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -22,7 +18,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.Toast;
-
 import com.eraldguri.geophysicslab.adapter.EarthquakeListAdapter;
 import com.eraldguri.geophysicslab.R;
 import com.eraldguri.geophysicslab.api.model.Features;
@@ -33,21 +28,18 @@ import com.eraldguri.geophysicslab.permissions.PermissionCallback;
 import com.eraldguri.geophysicslab.permissions.PermissionUtil;
 import com.eraldguri.geophysicslab.util.DateTimeUtil;
 import com.eraldguri.geophysicslab.util.DividerItemDecorator;
+import com.eraldguri.geophysicslab.util.GeoSnackBar;
 import com.eraldguri.geophysicslab.util.StringUtils;
 import com.google.android.material.snackbar.Snackbar;
-import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
-
 import org.jetbrains.annotations.NotNull;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class EarthquakeListFragment extends EarthquakesFragment implements
@@ -56,11 +48,15 @@ public class EarthquakeListFragment extends EarthquakesFragment implements
     private RecyclerView mEarthquakeListView;
     private EarthquakeListAdapter mEarthquakeListAdapter;
     private FrameLayout mFrameLayout;
-
-    private int itemPosition;
     private List<Features> earthquakes;
 
     private PermissionUtil permissionUtil;
+    private boolean isDirectoryCreated;
+    private boolean isCSVCreated;
+    private static final int SNACK_BAR_DURATION = 5000;
+    private boolean success = false;
+
+    int counter = 0;
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
@@ -86,79 +82,84 @@ public class EarthquakeListFragment extends EarthquakesFragment implements
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void exportToCSV(List<Features> features) {
        File root = Environment.getExternalStorageDirectory();
-       File dir = new File(root.getAbsolutePath() + "/GeophysicsLab");
-       if (!dir.exists()) {
-           dir.mkdirs();
-       } else {
-           dir.delete();
+       File dir = new File(root.getAbsolutePath() + StringUtils.csvDirectory);
+       if (!dir.exists() && !isDirectoryCreated) {
+           isDirectoryCreated = dir.mkdirs();
        }
-       File csvFile = new File(dir, "earthquakes.csv");
-        try {
-            FileWriter outputFile = new FileWriter(csvFile);
-            CSVWriter writer = new CSVWriter(outputFile);
-            String[] header = {
-                    "Place", "Felt", "Date/Time", "Magnitude", "Latitude", "Longitude", "Depth",
-                    "cdi", "mmi", "sig", "nst", "dmin", "rms", "gap", "Magnitude Type", "Tsunami"
-            };
-            writer.writeNext(header);
+       File csvFile = new File(dir, StringUtils.csvName + StringUtils.csvExtension);
+       if (csvFile.getName().equals(StringUtils.csvName + StringUtils.csvExtension)) {
+           StringUtils.replaceWithNewFileName(StringUtils.csvName + StringUtils.csvExtension, dir, StringUtils.csvExtension);
+           //GeoSnackBar.errorSnackBar(requireContext(), mFrameLayout, R.string.csv_exists, SNACK_BAR_DURATION);
+           success = false;
+       } else {
+           try {
+               FileWriter outputFile = new FileWriter(csvFile);
+               CSVWriter writer = new CSVWriter(outputFile);
+               writer.writeNext(StringUtils.header);
 
-            earthquakes = features;
-            for (int i = 0; i < features.size(); i++) {
-                double[] geometry = features.get(i).getGeometry().getCoordinates();
-                double longitude = geometry[0];
-                double latitude = geometry[1];
-                double depth = geometry[2];
+               for (int i = 0; i < features.size(); i++) {
+                   double[] geometry = features.get(i).getGeometry().getCoordinates();
+                   double longitude = geometry[0];
+                   double latitude = geometry[1];
+                   double depth = geometry[2];
 
-                String place = features.get(i).getProperties().getPlace();
-                double magnitude = features.get(i).getProperties().getMagnitude();
-                String felt = features.get(i).getProperties().getFelt();
+                   String place = features.get(i).getProperties().getPlace();
+                   double magnitude = features.get(i).getProperties().getMagnitude();
+                   String felt = features.get(i).getProperties().getFelt();
 
-                String dateTime = features.get(i).getProperties().getTime();
-                String formattedTime = DateTimeUtil.parseDateTimeFromString(dateTime);
+                   String dateTime = features.get(i).getProperties().getTime();
+                   String formattedTime = DateTimeUtil.parseDateTimeFromString(dateTime);
 
-                int tsunami = features.get(i).getProperties().getTsunami();
-                int tz = features.get(i).getProperties().getTz();
-                String cdi = features.get(i).getProperties().getCdi();
-                String mmi = features.get(i).getProperties().getMmi();
-                int sig = features.get(i).getProperties().getSig();
-                int nst = features.get(i).getProperties().getNst();
-                double dmin = features.get(i).getProperties().getDmin();
-                double rms = features.get(i).getProperties().getRms();
-                double gap = features.get(i).getProperties().getGap();
-                String magType = features.get(i).getProperties().getMagType();
+                   int tsunami = features.get(i).getProperties().getTsunami();
+                   int tz = features.get(i).getProperties().getTz();
+                   String cdi = features.get(i).getProperties().getCdi();
+                   String mmi = features.get(i).getProperties().getMmi();
+                   int sig = features.get(i).getProperties().getSig();
+                   int nst = features.get(i).getProperties().getNst();
+                   double dmin = features.get(i).getProperties().getDmin();
+                   double rms = features.get(i).getProperties().getRms();
+                   double gap = features.get(i).getProperties().getGap();
+                   String magType = features.get(i).getProperties().getMagType();
 
-                String _tsunami = " ";
-                if (tsunami == 1) {
-                    _tsunami = "Yes";
-                } else if (tsunami == 0) {
-                    _tsunami = " ";
-                }
-                writer.writeNext(new String[]{
-                        place,
-                        felt,
-                        formattedTime,
-                        String.valueOf(magnitude),
-                        String.valueOf(latitude),
-                        String.valueOf(longitude),
-                        String.valueOf(depth),
-                        cdi,
-                        mmi,
-                        String.valueOf(sig),
-                        magType,
-                        String.valueOf(gap),
-                        String.valueOf(rms),
-                        String.valueOf(dmin),
-                        String.valueOf(nst),
-                        String.valueOf(tz),
-                        magType,
-                        _tsunami
-                });
-            }
+                   String _tsunami = " ";
+                   if (tsunami == 1) {
+                       _tsunami = "Yes";
+                   } else if (tsunami == 0) {
+                       _tsunami = " ";
+                   }
+                   writer.writeNext(new String[]{
+                           place,
+                           felt,
+                           formattedTime,
+                           String.valueOf(magnitude),
+                           String.valueOf(latitude),
+                           String.valueOf(longitude),
+                           String.valueOf(depth),
+                           cdi,
+                           mmi,
+                           String.valueOf(sig),
+                           magType,
+                           String.valueOf(gap),
+                           String.valueOf(rms),
+                           String.valueOf(dmin),
+                           String.valueOf(nst),
+                           String.valueOf(tz),
+                           magType,
+                           _tsunami
+                   });
+               }
 
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+               writer.close();
+               if (success) {
+                   GeoSnackBar.successSnackBar(requireContext(), mFrameLayout, R.string.csv_created, SNACK_BAR_DURATION);
+               } else {
+                   GeoSnackBar.errorSnackBar(requireContext(), mFrameLayout, R.string.failed_to_export, SNACK_BAR_DURATION);
+               }
+
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
     }
 
     @Override
@@ -193,25 +194,32 @@ public class EarthquakeListFragment extends EarthquakesFragment implements
                 }
             }
         } else if (item.getItemId() == R.id.csv_export) {
-            if (permissionUtil.checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    && permissionUtil.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                exportToCSV(earthquakes);
+            return checkForStoragePermissions();
+        }
+
+        return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private boolean checkForStoragePermissions() {
+        if (permissionUtil.checkPermission(READ_EXTERNAL_STORAGE)
+                && permissionUtil.checkPermission(WRITE_EXTERNAL_STORAGE)) {
+            exportToCSV(earthquakes);
+        } else {
+            if (permissionUtil.shouldShowRequestPermissionRationale(requireActivity(), WRITE_EXTERNAL_STORAGE)) {
+                Snackbar.make(mFrameLayout,
+                        "GeophysicsLab will need to write to external storage to export as CSV",
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK", v ->
+                                permissionUtil.askForPermission(requireActivity(),
+                                        new String[] {
+                                                READ_EXTERNAL_STORAGE,
+                                                WRITE_EXTERNAL_STORAGE
+                                        }, mPermissionCallback)).show();
             } else {
-                if (permissionUtil.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    Snackbar.make(mFrameLayout,
-                            "GeophysicsLab will need to write to external storage to export as CSV",
-                            Snackbar.LENGTH_INDEFINITE)
-                            .setAction("OK", v ->
-                                    permissionUtil.askForPermission(requireActivity(),
-                                            new String[] {
-                                                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                                                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                            }, mPermissionCallback)).show();
-                } else {
-                    permissionUtil.askForPermission(requireActivity(), new String[] {
-                            Manifest.permission.READ_EXTERNAL_STORAGE,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE}, mPermissionCallback);
-                }
+                permissionUtil.askForPermission(requireActivity(), new String[] {
+                        READ_EXTERNAL_STORAGE,
+                        WRITE_EXTERNAL_STORAGE}, mPermissionCallback);
             }
         }
 
@@ -246,7 +254,7 @@ public class EarthquakeListFragment extends EarthquakesFragment implements
             mEarthquakeListView.addItemDecoration(new DividerItemDecorator(requireContext()));
             mEarthquakeListView.setAdapter(mEarthquakeListAdapter);
 
-            exportToCSV(earthquakes);
+            this.earthquakes = earthquakes;
         }
     }
 
---
+++ app/src/main/java/com/eraldguri/geophysicslab/util/GeoSnackBar.java
@@ -0,0 +1,23 @@
+package com.eraldguri.geophysicslab.util;
+
+import android.content.Context;
+import android.view.View;
+
+import com.eraldguri.geophysicslab.R;
+import com.google.android.material.snackbar.Snackbar;
+
+public class GeoSnackBar {
+
+    public static void successSnackBar(Context context, View view, int text, int duration) {
+        Snackbar snackbar = Snackbar.make(view, text, duration);
+        snackbar.setTextColor(context.getResources().getColor(R.color.green));
+        snackbar.show();
+    }
+
+    public static void errorSnackBar(Context context, View view, int text, int duration) {
+        Snackbar snackbar = Snackbar.make(view, text, duration);
+        snackbar.setTextColor(context.getResources().getColor(R.color.red));
+        snackbar.show();
+    }
+
+}
